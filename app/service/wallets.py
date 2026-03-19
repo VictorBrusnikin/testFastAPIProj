87b8fec2f032d097353524ffd28f166c77752dc5
Victor Brusnikin <viktorbrusnikin35@gmail.com>
2026-03-19 22:29:10 +0400

    Dependency

--- app/service/wallets.py
+++ app/service/wallets.py
@@ -1,29 +1,32 @@
 from fastapi import HTTPException
+from sqlalchemy.orm import Session
 
+from app.database import SessionLocal
 from app.schemas import CreateWalletRequest
 from app.repository import wallets as wallets_repository
 
 
-def get_wallet(wallet_name: str | None = None):
+def get_wallet(db: Session, wallet_name: str | None = None):
     if wallet_name is None:
-        wallets = wallets_repository.get_all_wallets()
-        return {"total_balance" : sum(wallets.values())}
-    if not wallets_repository.is_wallet_exist(wallet_name):
+        wallets = wallets_repository.get_all_wallets(db)
+        return {"total_balance" : sum([w.balance for w in wallets])}
+    if not wallets_repository.is_wallet_exist(db, wallet_name):
         raise HTTPException(
             status_code=404
         )
-    balance = wallets_repository.get_wallet_balance_by_name(wallet_name)
-    return {"wallet": wallet_name, "balance": balance}
+    wallet = wallets_repository.get_wallet_balance_by_name(db, wallet_name)
+    return {"wallet": wallet.name, "balance": wallet.balance}
 
-def create_wallet(wallet: CreateWalletRequest):
-    if wallets_repository.is_wallet_exist(wallet.name):
+def create_wallet(db: Session, wallet: CreateWalletRequest):
+    if wallets_repository.is_wallet_exist(db, wallet.name):
         raise HTTPException(
             status_code=400,
             detail=f"Wallet '{wallet.name}' already exists"
         )
-    balance = wallets_repository.create_wallet(wallet.name, wallet.initial_balance)
+    wallet = wallets_repository.create_wallet(db, wallet.name, wallet.initial_balance)
+    db.commit()
     return {
         "message": f"Wallet '{wallet.name}' created",
         "wallet": wallet.name,
-        "balance": balance,
+        "balance": wallet.balance,
     }
